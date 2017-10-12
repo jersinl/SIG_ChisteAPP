f2bd586b7307a794ceb7c694b945a0db1676cffb
jersinl <30739611+jersinl@users.noreply.github.com>
2017-10-12 03:31:08 -0500

    Segunda Prueba

--- app/src/main/java/solutions/thinker/chisteapp/splashScreen.java
+++ app/src/main/java/solutions/thinker/chisteapp/splashScreen.java
@@ -2,12 +2,17 @@ package solutions.thinker.chisteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class splashScreen extends AppCompatActivity {
+ImageButton txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
     }
 }
